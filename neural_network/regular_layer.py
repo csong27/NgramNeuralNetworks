9414432bbf5280e5e86dbd92f120f98842cf22ca
Congzheng Song <csong27@outlook.com>
2015-11-19 21:57:20 -0500

    update

--- neural_network/regular_layer.py
+++ neural_network/regular_layer.py
@@ -113,59 +113,45 @@ class LogisticRegression(object):
 
 class LeNetConvPoolLayer(object):
     """Pool Layer of a convolutional network """
-    def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
+
+    def __init__(self, rng, input, filter_shape, image_shape, activation, poolsize=(2, 2)):
 
         assert image_shape[1] == filter_shape[1]
         self.input = input
-
+        self.filter_shape = filter_shape
+        self.image_shape = image_shape
+        self.poolsize = poolsize
+        self.activation = activation
         # there are "num input feature maps * filter height * filter width"
         # inputs to each hidden unit
         fan_in = numpy.prod(filter_shape[1:])
         # each unit in the lower layer receives a gradient from:
         # "num output feature maps * filter height * filter width" /
         #   pooling size
-        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
-                   numpy.prod(poolsize))
+        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) / numpy.prod(poolsize))
         # initialize weights with random weights
-        W_bound = numpy.sqrt(6. / (fan_in + fan_out))
-        self.W = theano.shared(
-            numpy.asarray(
-                rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
-                dtype=theano.config.floatX
-            ),
-            borrow=True
-        )
-
-        # the bias is a 1D tensor -- one bias per output feature map
+        if "relu" in activation.func_name:
+            self.W = theano.shared(numpy.asarray(rng.uniform(low=-0.01,high=0.01, size=filter_shape),
+                                                 dtype=theano.config.floatX), borrow=True, name="W_conv")
+        else:
+            W_bound = numpy.sqrt(6. / (fan_in + fan_out))
+            self.W = theano.shared(numpy.asarray(rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
+                                                 dtype=theano.config.floatX), borrow=True, name="W_conv")
         b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
-        self.b = theano.shared(value=b_values, borrow=True)
+        self.b = theano.shared(value=b_values, borrow=True, name="b_conv")
 
         # convolve input feature maps with filters
-        conv_out = conv.conv2d(
-            input=input,
-            filters=self.W,
-            filter_shape=filter_shape,
-            image_shape=image_shape
-        )
-
-        # downsample each feature map individually, using maxpooling
-        pooled_out = downsample.max_pool_2d(
-            input=conv_out,
-            ds=poolsize,
-            ignore_border=True
-        )
-
-        # add the bias term. Since the bias is a vector (1D array), we first
-        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
-        # thus be broadcasted across mini-batches and feature map
-        # width & height
-        self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-
-        # store parameters of this layer
+        conv_out = conv.conv2d(input=input, filters=self.W, filter_shape=self.filter_shape, image_shape=self.image_shape)
+        conv_out_tanh = activation(conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+        self.output = downsample.max_pool_2d(input=conv_out_tanh, ds=self.poolsize, ignore_border=True)
         self.params = [self.W, self.b]
 
-        # keep track of model input
-        self.input = input
+    def predict(self, new_data, batch_size, kernel_size):
+        img_shape = (batch_size, kernel_size, self.image_shape[2], self.image_shape[3])
+        conv_out = conv.conv2d(input=new_data, filters=self.W, filter_shape=self.filter_shape, image_shape=img_shape)
+        conv_out_tanh = self.activation(conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+        output = downsample.max_pool_2d(input=conv_out_tanh, ds=self.poolsize, ignore_border=True)
+        return output
 
 
 def binary_hinge_loss(predictions, targets, binary=True, delta=1):
